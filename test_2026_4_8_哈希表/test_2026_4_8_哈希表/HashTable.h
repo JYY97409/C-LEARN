78b08f52e54f4ae69785e1af0209d8b4588a9d21
JYY97409 <52705803@qq.com>
2026-04-11 22:58:39 +0800

    2026_4_11_哈希表（不完善）

---
+++ test_2026_4_8_哈希表/test_2026_4_8_哈希表/HashTable.h
@@ -0,0 +1,110 @@
+#pragma once
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+enum State
+{
+	EXIST,
+	EMPTY,
+	DELETE
+};
+
+
+template<class K, class V>
+class HashData
+{
+	
+
+public:
+	pair<K, V> _data;
+	State _state = EMPTY;
+};
+
+
+template<class K, class V>
+class HashTable
+{
+public:
+	HashTable(int size = 11)
+		:_table(size)
+		, _n(0)
+	{}
+
+	bool insert(const pair<K,V>& kv)
+	{
+		if (_n * 10 / _table.size() > 7)
+		{
+			HashTable<K, V> new_hash(_table.size()*2);
+
+			for (auto e : _table)
+			{
+				new_hash.insert(e._data);
+			}
+
+			_table.swap(new_hash._table);
+			_n = new_hash._n;
+		}
+
+		int hash0 = kv.first % _table.size();
+		int i = 1;
+		int hashi = hash0;
+		while (_table[hashi]._state == EXIST)
+		{
+			//当该位置是有数据的时候是需要移动的
+			hashi = (hashi + i) % _table.size();
+			i++;
+		}
+		
+		_table[hashi]._data = kv;
+		_table[hashi]._state = EXIST;
+		_n++;
+		return true;
+	}
+
+
+	//整体就是先取模，然后++
+	//这里不是vector 的指针是因为vector实际上是顺序表，不是数组
+	HashData<K, V>* find(const K& key)
+	{
+		int hash0 = key % _table.size();
+		if (_table[hash0]._n == EXIST && _table[hash0]._data.first == key)
+		{
+			return &_table[hash0];
+		}
+		int i = 1;
+		int hashi = hash0;
+		while (_table[hashi]._state == EXIST)
+		{
+			if (_table[hashi]._data.first == key)
+			{
+				return &_table[hashi];
+			}
+			//当该位置是有数据的时候是需要移动的
+			hashi = (hashi + i) % _table.size();
+			i++;
+		}
+
+
+		return nullptr;
+	}
+
+	bool erase(const K& key)
+	{
+		HashData<K, V>* kv = find(key);
+
+		if (kv == nullptr)
+			return false;
+		else
+		{
+			kv->_state = EXIST;
+			--_n;
+			return true;
+		}
+	}
+private:
+	vector <HashData<K,V>> _table;
+	size_t _n;
+};
--- test_2026_4_8_哈希表/test_2026_4_8_哈希表/test.cpp
+++ test_2026_4_8_哈希表/test_2026_4_8_哈希表/test.cpp
@@ -1,1 +1,24 @@
-//
+#include<iostream>
+#include<vector>
+#include"HashTable.h"
+using namespace std;
+
+void test_hash1()
+{
+	HashTable<int, int > h1;
+	int a[] = { 19,30,5,36,13,20,21,12 };
+	for (auto e : a)
+	{
+		h1.insert({e,e});
+	}
+	
+
+}
+
+
+int main()
+{
+	test_hash1();
+	
+	return 0;
+}
